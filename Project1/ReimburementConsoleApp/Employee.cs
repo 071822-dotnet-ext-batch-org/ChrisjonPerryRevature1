70be01e4bbd2d977e671663b8fe882de3888f729
ChrisjonPerry <chrisjon350@revature.net>
2022-08-06 11:57:11 -0400

    update

---
+++ Project1/ReimburementConsoleApp/Employee.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReimburementConsoleApp
+{
+    public class Employee
+    {
+    public string EmployeeName { get; set; }
+    public string EmployeePhone { get; set; }
+    public string EmployeeUsername { get; set; }
+    public string EmployeeID { get; set; }
+    }
+}
---
+++ Project1/ReimburementConsoleApp/ReimbursementSystem.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReimburementConsoleApp
+{
+   
+    public class ReimbursementSystem
+    {s
+
+    public static void WelcomeUser(){
+    System.Console.WriteLine("Welcome to Reimbursiment+ ! \nHere you can submit a claim for reimbursement ! \nafter you have requested reimbursemnt a manager will accept or deny your request. \npress enter to continue");
+    }
+    public void Setup(){
+        System.Console.WriteLine(" ");
+    }    
+    }
+}
