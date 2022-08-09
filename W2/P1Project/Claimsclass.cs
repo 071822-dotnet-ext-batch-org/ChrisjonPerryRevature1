153cac94a16d526ec5f7a50c74fcfc7f3cf0a303
ChrisjonPerry <chrisjon350@revature.net>
2022-08-09 17:31:20 -0400

    Adding files

---
+++ W2/P1Project/Claimsclass.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P1Project
+{
+    public class Claimsclass
+    {
+    // list for amoountsfor amounts 
+     
+     
+    //amount
+    public int amountToReimburse { get; set; }
+    //denied or approved
+    public bool isApproved { get; set;} 
+          
+    }
+}
--- W2/P1Project/Program.cs
+++ W2/P1Project/Program.cs
@@ -11,23 +11,43 @@ namespace P1Project
 
 /// /////Prompts user to enter a E for new user or P for returning user must type valid input ////////////// 
 
-        System.Console.WriteLine("Please enter E for employee or M for manager");
+        System.Console.WriteLine("Please enter E to register as an employee or M as a manager \nor type R to login as an employee or N to login as a manager.");
+        RegisterUser reg = new RegisterUser ();
         string isEmpOrMng = Console.ReadLine();
         //Checks if user is an Employee or a manger
-        if (isEmpOrMng.Contains("E" )||isEmpOrMng.Contains("e")){
-            System.Console.WriteLine("Welcome valued new employee!");
-            RegisterUser newEmp = new RegisterUser();
-            newEmp.SetUpUser();
-            newEmp.Getimbursement(); 
-            }
-        
-        if (isEmpOrMng.Contains("m")||isEmpOrMng.Contains("M")){
-        
-        System.Console.WriteLine("Welcome new manger");
-       RegisterUser newmng = new RegisterUser();
-        newmng.SetupManager();
+       if(isEmpOrMng.Contains("E")||isEmpOrMng.Contains("e")){
+        reg.SetUpUser();
+       }
+         
+         if(isEmpOrMng.Contains("R")||isEmpOrMng.Contains("r")){
+        reg.LoginEmp();
+        System.Console.WriteLine("Please type 1 to view Reimburment claims or 2 make a claim");
+        var userInput = Console.ReadLine();
+        if(userInput.Contains("1")){
+          reg.ViewClaims();
+        } else if (userInput.Contains("2"))
+        {
+          reg.Getimbursement();
         }
+       }
+        
+         if(isEmpOrMng.Contains("M")||isEmpOrMng.Contains("m")){
+        reg.SetupManager();
+        reg.ViewClaims();
+        reg.approveClaim();
+
+       }
+        if(isEmpOrMng.Contains("N")||isEmpOrMng.Contains("n")){
+        reg.SetupManager();
+        reg.ViewClaims();
+        reg.approveClaim();
+        
+       }
+       else {
+        System.Console.WriteLine("Please enter a valid entry");
+       }
        
         }
     }
 }
+
